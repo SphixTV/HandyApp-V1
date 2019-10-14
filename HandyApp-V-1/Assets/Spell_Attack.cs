84b2a11e51d3a93f8a1cba8b7d66d086e1d7e966
Max <maxj-rothe@web.de>
2019-10-14 15:46:52 +0200

    sdg
    dfg

---
+++ HandyApp-V-1/Assets/Spell_Attack.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spell_Attack : MonoBehaviour
+{
+    public Spell_Dictonary spell_Dictonary;
+    public int Spell;
+    public float Speed;
+    public Transform shootPoint;
+    Animator animator;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            StartCoroutine(shoot());
+            animator.SetTrigger("attack1");
+        }
+    }
+    IEnumerator shoot()
+    {
+        yield return new WaitForSeconds(0.9F);
+        var SpellObject = Instantiate(spell_Dictonary.GetGameObject(Spell), shootPoint.position, transform.rotation);
+        SpellObject.GetComponent<Rigidbody>().AddForce(gameObject.transform.forward * Speed);
+        
+    }
+}
